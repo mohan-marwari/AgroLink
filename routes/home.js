521c13243e5bf6e0581d0325ae8075a5262d5b67
Mohan Lal <mohanmarwadi2023@gmail.com>
2025-02-01 21:59:38 +0530

    update home route

--- routes/home.js
+++ routes/home.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 
-
 //Index Route
 router.get(
-    "/",
-    wrapAsync(async (req, res) => {
-      res.render("listings/home.ejs");
-    })
-  );
+  "/",
+  wrapAsync(async (req, res) => {
+    // res.render("listings/home.ejs");
+    // const allListings = await Listing.find({});
+    // res.render("listings/index.ejs", { allListings });
+    res.redirect("/listings");
+  })
+);
 
-  module.exports = router;
+module.exports = router;
